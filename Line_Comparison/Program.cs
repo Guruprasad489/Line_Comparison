96b226b3cf9555ed160e5648bfac8db1e8b5d57b
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-23 13:12:01 +0530

    [Guru] Added LineComparison.cs to calculate length of line -UC1

--- Line_Comparison/Program.cs
+++ Line_Comparison/Program.cs
@@ -11,6 +11,8 @@ namespace Line_Comparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Problem \n");
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.LineLength();
         }
     }
 }
