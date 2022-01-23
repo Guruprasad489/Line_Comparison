5959fa2480decb280b8c54d4d12c1311468cb1bb
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-23 15:38:36 +0530

    [Guru] Modified to echeck equality of length of two lines

--- Line_Comparison/LineComparison.cs
+++ Line_Comparison/LineComparison.cs
@@ -8,10 +8,10 @@ namespace Line_Comparison
 {
     class LineComparison
     {
-        public double x1, x2, y1, y2, length;
+        double x1, x2, y1, y2, x3, x4, y3, y4, line1_Length, line2_Length;
         public void LineLength()
         {
-            Console.WriteLine("Please Enter x , y Co-ordinates of a line :");
+            Console.WriteLine("Please Enter x , y Co-ordinates of line 1 :");
             Console.WriteLine("Please Enter values of x1 and y1");
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
@@ -19,8 +19,25 @@ namespace Line_Comparison
             x2 = Convert.ToInt32(Console.ReadLine());
             y2 = Convert.ToInt32(Console.ReadLine());
 
-            length = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2-y1),2));
-            Console.WriteLine("Length of the line is : " +length);
+            Console.WriteLine("Please Enter x , y Co-ordinates of line 2 :");
+            Console.WriteLine("Please Enter values of x3 and y3");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter values of x4 and y4");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            line1_Length = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2-y1),2));
+            Console.WriteLine("Length of the line is : " + line1_Length);
+
+            line2_Length = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Length of the line is : " + line2_Length);
+
+            if (line1_Length == line2_Length)
+                Console.WriteLine("Length of line 1 and line 2 ane equal");
+            else
+                Console.WriteLine("Length of line 1 and line 2 ane not equal");
+
         }
     }
 }
